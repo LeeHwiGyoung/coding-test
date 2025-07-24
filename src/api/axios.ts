bf73f10dc9503e9f7478319ee4e6bde9187c1f35
HwiGyoung Lee <ter0000@naver.com>
2025-07-24 11:30:26 +0900

    ✨ feat(axios) : axiosInstance 설정

---
+++ src/api/axios.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  timeout: 1000,
+  headers: {'Content-Type' : 'application/json'}
+});
+
