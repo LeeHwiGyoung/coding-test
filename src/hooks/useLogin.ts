fddc9d5d65892039202048541ada9c748be4a1be
HwiGyoung Lee <ter0000@naver.com>
2025-07-24 11:31:55 +0900

    ✨ feat(useLogin) : loading , error , loign 함수를 제공하는 useLogin 훅

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,32 @@
+'use client';
+import { postLogin } from "@/api/endpoints/login";
+import { useState } from "react";
+import axios from "axios";
+
+export function useLogin() {
+    const [loading , setLoading] = useState<boolean>(false);
+    const [error , setError] = useState<string|null>(null); 
+    
+    const login = async(email : string, password : string) => {
+        setLoading(true);
+        setError(null);
+        try {
+            const accessToken = await postLogin(email,password);
+            localStorage.setItem('accessToken' , accessToken);
+            console.log('로그인 성공');
+            return true;
+        }
+        catch(error) {
+            if(axios.isAxiosError(error)){
+                setError(error.message);
+                console.error("로그인 실패" ,error)
+            }
+            localStorage.removeItem('accessToken');
+            return false;
+        }
+        finally {
+            setLoading(false);    
+        }
+    }
+    return {login , loading, error};
+}
